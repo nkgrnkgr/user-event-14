061925f62e608b812bbe8094cae4f26a836eb98f
taiki-nishi <taiki-nishi@cybozu.co.jp>
2022-06-14 13:50:01 +0900

    testをv13動くように修正

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,12 +5,10 @@ import App from "./App";
 test("renders learn react link", async () => {
   render(<App />);
 
-  const user = userEvent.setup();
-
   const button = await screen.findByRole("button");
   button.focus();
 
-  await user.keyboard("{space}");
+  await userEvent.keyboard("{space}");
 
   const dialog = await screen.findByRole("dialog");
 
